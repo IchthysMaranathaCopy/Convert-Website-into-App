733824b406ab2a9c1d8f31544b5ecc10a21ded5e
IchthysMaranathaCopy <servantofchristjesus@yahoo.com>
2024-09-14 17:42:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,29 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:nowa_runtime/nowa_runtime.dart';
 import 'package:flutter/material.dart';
-import 'package:vragger/src/app.dart';
+import 'package:tst/start.dart';
 
-void main() => runApp(App());
+@NowaGenerated()
+late final SharedPreferences sharedPrefs;
+
+@NowaGenerated()
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  sharedPrefs = await SharedPreferences.getInstance();
+
+  runApp(const MyApp());
+}
+
+@NowaGenerated()
+class MyApp extends StatelessWidget {
+  @NowaGenerated({'loader': 'auto-constructor'})
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: 'start',
+      routes: {'start': (context) => const start()},
+    );
+  }
+}
